8e17f17d754cb558acb324ee939022d0431e06a8
lijun <lijun359714823@163.com>
2016-09-09 02:08:15 +0800

    [M001][lijun] - modify SwagerConfig

--- src/main/java/com/ifox/configuration/SwaggerConfig.java
+++ src/main/java/com/ifox/configuration/SwaggerConfig.java
@@ -16,7 +16,7 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("tw.thoughtpos.controller"))
+                .apis(RequestHandlerSelectors.basePackage("com.ifox.controller"))
                 .paths(PathSelectors.any())
                 .build();
     }
